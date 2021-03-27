89cda8401886b13248e6e337524306159b87080d
Eva Du Lieu <eve.dulieu@gmail.com>
2021-03-27 20:30:13 +0000

    Finished code and tested

--- byyear.sh
+++ byyear.sh
@@ -4,31 +4,51 @@
 # it assumes the directory name has the year in it like this (xxxx) e.g. (1956)
 #
 # param $1 is from directory
-# param $2 is to directory
-# param $3 set to 1 to acutally copy or 0 to pre-check
-# param $4 is the unquie id for this run
+# param $2 set to 1 to acutally copy or 0 to pre-check
+# param $3 is the unquie id for this run
 #
-#                      $1                                 $2             $3 $4
-# ./doit.sh /mnt/share/allmovies/Alphabetical/X /media/eva/Movie-Backup-1 1 X
-# ./doit.sh /mnt/share/movies/2020-11-November-1/ /media/eva/MovieWork/ 0 20111
+#                      $1                            $2     $3 
+# ./byyear.sh /mnt/share/allmovies/Alphabetical/X     1     X
+# ./byyear.sh /mnt/share/movies/2020-11-November-1/   0     20111
 
 # Need to add validation for input here
 
+targetDrive=""
+
+function _calcTargetDrive {
+
+    #echo $1
+
+    if [[ "$1" -ge 1900 ]]; then
+        if [[ "$1" -le 1979 ]]; then
+            #echo "MovieMaster1";
+            targetDrive="/media/eva/MovieMaster1"
+            return 
+        fi
+        if [[ "$1" -le 2025 ]]; then
+            #echo "MovieMaster2";
+            targetDrive="/media/eva/MovieMaster2"
+            return
+        fi
+    else
+        targetDrive="error";
+    fi
+}
+
+#_calcTargetDrive "1920"
+#echo $targetDrive
+#_calcTargetDrive "1950"
+#echo $targetDrive
+#_calcTargetDrive "2000"
+#echo $targetDrive
+#_calcTargetDrive "2020"
+#echo $targetDrive
+#_calcTargetDrive "1974"
+#echo $targetDrive
+
+#exit 0
 
 PASSED=$1
-
-if [ -d "${PASSED}" ] ; then
-    echo "$PASSED is a directory";
-else
-    if [ -f "${PASSED}" ]; then
-        echo "${PASSED} is a file";
-    else
-        echo "${PASSED} is not valid";
-        exit 1
-    fi
-fi
-
-PASSED=$2
 
 if [ -d "${PASSED}" ] ; then
     echo "$PASSED is a directory";
@@ -48,7 +68,7 @@ errcnt=0
 cnt=0
 dups=0
 
-ls $1 -xN1 > files-$4.txt
+ls $1 -xN1 > files-$3.txt
 
 while IFS="" read -r p || [ -n "$p" ]
 do
@@ -62,21 +82,25 @@ do
 
         if [[ "$p" == *"($i)"* ]]; then
 
+            _calcTargetDrive $i
+
+            #echo $targetDrive
+
             # set to and from directories
             from="$1/$p"
-            to="$2/$i/$p"
+            to="$targetDrive/$i/$p"
 
             ((cnt=cnt+1))
 
             # check if year directory exists
-            if [ ! -d "$2/$i" ] 
+            if [ ! -d "$targetDrive/$i" ] 
             then
                 # is it a test run?
-                if [ $3 -eq 1 ]
+                if [ $2 -eq 1 ]
                 then
                     # nope
-                    echo "Creating Directory $2/$i"
-                    mkdir "$2"/"$i"
+                    echo "Creating Directory $targetDrive/$i"
+                    mkdir "$targetDrive"/"$i"
                 fi
             fi
             
@@ -85,7 +109,7 @@ do
             then
             
                 echo "$cnt >>>>>>>>>>>>>> file $to already exists." 
-                echo "$cnt >>>>>>>>>>>>>> file $to exists." >> ErrorExists-$4.txt
+                echo "$cnt >>>>>>>>>>>>>> file $to exists." >> ErrorExists-$3.txt
                 ((dups=dups+1))
            
             else
@@ -93,7 +117,7 @@ do
                 echo "$cnt coping file $from -> $to "
             
                 # is it a test run?
-                if [ $3 -eq 1 ]
+                if [ $2 -eq 1 ]
                 then
                     # nope
                     cp -R -- "$from" "$to"
@@ -105,9 +129,9 @@ do
                 # did we get an error
                 if [ $? -ne 0 ]
                 then
-                    echo "$cnt Error copying $from" >> Erroroutputfile-$4.txt
+                    echo "$cnt Error copying $from" >> Erroroutputfile-$3.txt
                 else
-                    echo "$cnt copied $from" >> FilesProcessed-$4.txt
+                    echo "$cnt copied $from" >> FilesProcessed-$3.txt
                     found=1
                     break 
                 fi
@@ -121,13 +145,15 @@ do
     then
         # nope
         ((errcnt=errcnt+1))
-        echo "Not copied $p" >> FilesNOTProcessed-$4.txt
+        echo "Not copied $p" >> FilesNOTProcessed-$3.txt
     fi
 
-done < files-$4.txt
+done < files-$3.txt
 
 echo "========================================="
 echo "Number movie directories that already exist $dups"
 echo "Number movie directories with issues $errcnt"
 
 echo "Complete"
+
+exit 0
